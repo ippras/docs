1dcb32a769f88e0b0b713852931c8fa981c52bef
kgv <kgv@users.noreply.github.com>
2026-04-27 17:09:55 +0300

    1

--- src/fatty_acids/mod.rs
+++ src/fatty_acids/mod.rs
@@ -2,12 +2,17 @@
 
 use crate::asset;
 
-// Factors
-pub const ENRICHMENT_FACTOR: &str = asset!("/assets/en/fatty_acids/EnrichmentFactor.md");
-pub const SELECTIVITY_FACTOR: &str = asset!("/assets/en/fatty_acids/SelectivityFactor.md");
+pub const CONJUGATED: &str = asset!("/assets/en/fatty_acids/Conjugated.md");
+pub const MONOUNSATURATED: &str = asset!("/assets/en/fatty_acids/Monounsaturated.md");
+pub const OMEGA_3: &str = asset!("/assets/en/fatty_acids/Omega-3.md");
+pub const OMEGA_6: &str = asset!("/assets/en/fatty_acids/Omega-6.md");
+pub const OMEGA_9: &str = asset!("/assets/en/fatty_acids/Omega-9.md");
+pub const POLYUNSATURATED: &str = asset!("/assets/en/fatty_acids/Polyunsaturated.md");
+pub const SATURATED: &str = asset!("/assets/en/fatty_acids/Saturated.md");
+pub const TRANS: &str = asset!("/assets/en/fatty_acids/Trans.md");
+pub const UNSATURATED: &str = asset!("/assets/en/fatty_acids/Unsaturated.md");
 
-// Indices
-// pub const CONJUGATED: &str = asset!("/assets/en/fatty_acids/Conjugated.md");
+// Complex
 pub const EICOSAPENTAENOIC_AND_DOCOSAHEXAENOIC: &str = asset!("/assets/en/fatty_acids/EicosapentaenoicAndDocosahexaenoic.md");
 pub const FISH_LIPID_QUALITY: &str = asset!("/assets/en/fatty_acids/FishLipidQuality.md");
 pub const HEALTH_PROMOTING_INDEX: &str = asset!("/assets/en/fatty_acids/HealthPromotingIndex.md");
@@ -17,7 +22,10 @@ pub const INDEX_OF_THROMBOGENICITY: &str = asset!("/assets/en/fatty_acids/IndexO
 pub const LINOLEIC_TO_ALPHA_LINOLENIC: &str = asset!("/assets/en/fatty_acids/LinoleicToAlphaLinolenic.md");
 pub const POLYUNSATURATED_6_TO_POLYUNSATURATED_3: &str = asset!("/assets/en/fatty_acids/Polyunsaturated-6ToPolyunsaturated-3.md");
 pub const POLYUNSATURATED_TO_SATURATED: &str = asset!("/assets/en/fatty_acids/PolyunsaturatedToSaturated.md");
-pub const TRANS: &str = asset!("/assets/en/fatty_acids/Trans.md");
 pub const UNSATURATION_INDEX: &str = asset!("/assets/en/fatty_acids/UnsaturationIndex.md");
+
+// Factors
+pub const ENRICHMENT_FACTOR: &str = asset!("/assets/en/fatty_acids/EnrichmentFactor.md");
+pub const SELECTIVITY_FACTOR: &str = asset!("/assets/en/fatty_acids/SelectivityFactor.md");
 
 pub mod biodiesel;

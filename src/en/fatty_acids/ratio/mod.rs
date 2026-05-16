b78a0279fdb34a8f06792475e1a61f7be44eb3b8
kgv <kgv@users.noreply.github.com>
2026-05-16 22:28:59 +0300

    Biodiesel

--- src/en/fatty_acids/ratio/mod.rs
+++ src/en/fatty_acids/ratio/mod.rs
@@ -4,12 +4,15 @@ use crate::asset;
 
 // Biodiesel
 
-// pub const CETANE_NUMBER: &str = asset!("en/en/fatty_acids/biodiesel/CetaneNumber.md");
-// pub const COLD_FILTER_PLUGGING_POINT: &str = asset!("en/en/fatty_acids/biodiesel/ColdFilterPluggingPoint.md");
-// pub const DEGREE_OF_UNSATURATION: &str = asset!("en/en/fatty_acids/biodiesel/DegreeOfUnsaturation.md");
-// pub const IODINE_VALUE: &str = asset!("en/en/fatty_acids/biodiesel/IodineValue.md");
-// pub const LONG_CHAIN_SATURATED_FACTOR: &str = asset!("en/en/fatty_acids/biodiesel/LongChainSaturatedFactor.md");
-// pub const OXIDATION_STABILITY: &str = asset!("en/en/fatty_acids/biodiesel/OxidationStability.md");
+pub const CETANE_NUMBER: &str = asset!("en/FattyAcids/Ratio/Biodiesel/CetaneNumber.md");
+pub const COLD_FILTER_PLUGGING_POINT: &str =
+    asset!("en/FattyAcids/Ratio/Biodiesel/ColdFilterPluggingPoint.md");
+pub const DEGREE_OF_UNSATURATION: &str =
+    asset!("en/FattyAcids/Ratio/Biodiesel/DegreeOfUnsaturation.md");
+pub const IODINE_VALUE: &str = asset!("en/FattyAcids/Ratio/Biodiesel/IodineValue.md");
+pub const LONG_CHAIN_SATURATED_FACTOR: &str =
+    asset!("en/FattyAcids/Ratio/Biodiesel/LongChainSaturatedFactor.md");
+pub const OXIDATION_STABILITY: &str = asset!("en/FattyAcids/Ratio/Biodiesel/OxidationStability.md");
 
 // Metabolic
 

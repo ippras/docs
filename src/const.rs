742102d2c227e66656c8372b9ab441af7a1c713f
kgv <kgv@users.noreply.github.com>
2026-05-17 14:20:40 +0300

    const

--- src/const.rs
+++ src/const.rs
@@ -5,3 +5,5 @@ pub const SUM: &str = "Sum";
 pub const BIODIESEL: &str = "Biodiesel";
 pub const METABOLIC: &str = "Metabolic";
 pub const NUTRITIONAL: &str = "Nutritional";
+
+pub const CETANE_NUMBER: &str = "CetaneNumber";
